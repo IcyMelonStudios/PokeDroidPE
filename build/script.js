857b35155fea23476df54153615ab54adf1189fa
Akharote1 <adityakharote@hotmail.com>
2016-03-05 17:25:44 +0530

    Added PokemonType!

--- build/script.js
+++ build/script.js
@@ -132,7 +132,26 @@ var pokeMaxExp = [0, 1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 1331, 1728, 21
 var MediaPlayer = android.media.MediaPlayer();
 
 
-
+var PokemonType = {
+	NORMAL: 0,
+	FIGHTING: 1,
+	FLYING: 2,
+	POISON: 3,
+	GROUND: 4,
+	ROCK: 5,
+	BUG: 6,
+	GHOST: 7,
+	STEEL: 8,
+	FIRE: 9,
+	WATER: 10,
+	GRASS: 11,
+	ELECTRIC: 12,
+	PSYCHIC: 13,
+	ICE: 14,
+	DRAGON: 15,
+	DARK: 16,
+	FAIRY: 17
+}
 
 
 
